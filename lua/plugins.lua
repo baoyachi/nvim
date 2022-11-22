3351649ddfd07459c89ff82701f2baa170e9d9c8
baoyachi <liaoymxsdl@gmail.com>
2022-11-23 00:09:27 +0800

    add nvim plugin conf

---
+++ lua/plugins.lua
@@ -0,0 +1,16 @@
+local packer = require("packer")
+packer.startup(
+  function(use)
+   -- Packer 可以管理自己本身
+   use 'wbthomason/packer.nvim'
+end)
+
+pcall(
+  vim.cmd,
+  [[
+    augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerSync
+    augroup end
+  ]]
+)

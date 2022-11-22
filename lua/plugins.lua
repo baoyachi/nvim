023eb86cd68f15d3cfacf3fe7058692a893c7e0b
baoyachi <liaoymxsdl@gmail.com>
2022-11-23 00:42:05 +0800

    add nvim-tree plugin

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -3,8 +3,11 @@ packer.startup(
   function(use)
    -- Packer 可以管理自己本身
    use 'wbthomason/packer.nvim'
-  --  安装tokyonight主题
+   --  安装tokyonight主题
     use("folke/tokyonight.nvim")
+   --  安装侧边栏
+    use({ "nvim-tree/nvim-tree.lua", requires = "nvim-tree/nvim-web-devicons" })
+
 end)
 
 pcall(

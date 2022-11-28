d26a5fe050386a8efb35586f57a1f3cbbd1c9fea
baoyachi <liaoymxsdl@gmail.com>
2022-11-29 00:30:49 +0800

    add nvim-treesitter plugin

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -14,6 +14,14 @@ packer.startup(
     use("arkav/lualine-lsp-progress")
     -- telescope 
     use { 'nvim-telescope/telescope.nvim', requires = { "nvim-lua/plenary.nvim" } } 
+    -- nvim-treesitter
+    use {
+        'nvim-treesitter/nvim-treesitter',
+        run = function()
+            local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+            ts_update()
+        end,
+    }
 end)
 
 pcall(
